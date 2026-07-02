54b12860f2a6c53482856fd2d448a15be2a8b651
David Gelmà Corral <davidgelmacorral@gmail.com>
2026-07-02 12:02:49 +0200

    feat: Connect to Neon PostgreSQL with connection pooling

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+const {Pool} = require('pg');
+
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
+const pool = new Pool({connectionString: process.env.DATABASE_URL});
 
 app.get("/health", (req, res) => {
     res.status(200).send("OK");
@@ -9,3 +15,9 @@ app.get("/health", (req, res) => {
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
 });
+
+async function testConnection() {
+    const res = await pool.query('SELECT NOW()')
+    console.log(res);
+}
+testConnection();
